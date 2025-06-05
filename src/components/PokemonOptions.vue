29fdf930e2f94fd8378dee3138114e8915043f25
Emil2245 <emilmontiel@hotmail.com>
2025-06-04 19:39:47 -0500

    Taller22

--- src/components/PokemonOptions.vue
+++ src/components/PokemonOptions.vue
@@ -2,35 +2,43 @@
   <!-- <h1>Pokemon Options</h1> -->
   <div class="options-container">
     <ul>
-      <li>Pokemon 1</li>
-      <li>Pokemon 2</li>
-      <li>Pokemon 3</li>
-      <li>Pokemon 4</li>
+      <li v-for="pokemon in pokemons" :key="pokemon.id">{{ pokemon.nombre }}</li>
     </ul>
   </div>
 </template>
 
 <script>
-  export default {};
+export default {
+  props: {
+    pokemons: {
+      type: Array,
+      required: true,
+    }
+  },
+  
+};
 </script>
 
 <style>
-  ul {
-    list-style-type: none;
-  }
-  li {
-    background: white;
-    border-radius: 5px;
-    border: 1px solid black;
-    cursor: pointer;
-    margin-bottom: 10px;
-    width: 250px;
-  }
-  .options-container {
-    display: flex;
-    justify-content: center;
-  }
-  li:hover {
-    background-color: lightblue;
-  }
+ul {
+  list-style-type: none;
+}
+
+li {
+  background: white;
+  border-radius: 5px;
+  border: 1px solid black;
+  cursor: pointer;
+  margin-bottom: 10px;
+  width: 250px;
+}
+
+.options-container {
+  display: flex;
+  justify-content: center;
+}
+
+li:hover {
+  background-color: lightblue;
+}
 </style>
--- src/views/PokemonView.vue
+++ src/views/PokemonView.vue
@@ -1,18 +1,35 @@
 <template>
   <h1>Selecciona el Pokemon</h1>
-  <PokemonImage :pokemonId="4" :mostrarImagen="true" />
-  <PokemonOptions />
+  <PokemonImage :pokemonId="4" :mostrarImagen="false" />
+  <PokemonOptions :pokemons="this.vectorPokemon" />
 </template>
 
 <script>
-  import PokemonImage from "@/components/PokemonImage.vue";
-  import PokemonOptions from "@/components/PokemonOptions.vue";
-  export default {
-    components: {
-      PokemonImage,
-      PokemonOptions,
-    },
-  };
+import { obtenerOpcionesFachada } from '@/clients/PokemonClient'
+
+import PokemonImage from "@/components/PokemonImage.vue";
+import PokemonOptions from "@/components/PokemonOptions.vue";
+
+export default {
+  data() {
+    return{
+      vectorPokemon: []
+    }
+  },
+  components: {
+    PokemonImage,
+    PokemonOptions,
+  },
+  methods: {
+    async iniciarJuego() {
+      this.vectorPokemon = await obtenerOpcionesFachada(4)
+      // console.log(opciones)
+    }
+  },
+  mounted() {
+    this.iniciarJuego();
+  }
+};
 </script>
 
 <style></style>
